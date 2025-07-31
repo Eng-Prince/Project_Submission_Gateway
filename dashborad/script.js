5b766956c9f2dfb1b344cd02c41ccc4f8799011d
Prince <Princevish.2374@gmail.com>
2025-08-01 02:34:30 +0530

    Some Modifications

---
+++ dashborad/script.js
@@ -0,0 +1,24 @@
+
+
+
+    // import Chart from 'chart.js/auto';
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [100, 19, 3, 500, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
